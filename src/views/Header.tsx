88ba65f2ece84d015d52905fed14163ac50d8435
Jessie Ji <jessunix@gmail.com>
2022-07-20 15:45:28 +0900

    refactor: turn view component styles into modules

--- src/views/Header.tsx
+++ src/views/Header.tsx
@@ -1,4 +1,4 @@
-import './Header.css'
+import $style from './Header.module.css'
 
 import ParallaxItem from '@/components/ParallaxItem'
 
@@ -9,37 +9,37 @@ import purple_shorts from '@/assets/purple_shorts.svg'
 
 function Header() {
   return(
-    <header className="header" id="cover">
-    <div className="header-bg"></div>
-    <div className="header-content">
-      <a className="header-logo"
-        href="/"
-        rel="noopener noreferrer"
-        title="Pelican Stylist"
-      >
-        <img alt="logo"
-          srcSet={`${logo_large} 658w, ${logo_small} 296w`}
-          sizes="(max-width: 600px) 90vw, 60vw"
-          src={logo_large} />
-      </a>
-      <div className="header-button">
-        {/* TODO: smooth scroll with el.scrollIntoView({ behavior: 'smooth' }) */}
-        <a className="button" href="#customize">explore</a>
+    <header className={$style.header} id="cover">
+      <div className={$style.bg}></div>
+      <div className={$style.content}>
+        <a className={$style.logo}
+          href="/"
+          rel="noopener noreferrer"
+          title="Pelican Stylist"
+        >
+          <img alt="logo"
+            srcSet={`${logo_large} 658w, ${logo_small} 296w`}
+            sizes="(max-width: 600px) 90vw, 60vw"
+            src={logo_large} />
+        </a>
+        <div className={$style.cta}>
+          {/* TODO: smooth scroll with el.scrollIntoView({ behavior: 'smooth' }) */}
+          <a className="button" href="#customize">explore</a>
+        </div>
+        <div className={$style.hero}>
+        <HeroImg />
+        </div>
       </div>
-      <div className="header-hero">
-      <HeroImg />
+      <div className={$style.parallax}>
+        <ParallaxItem scale={2} value={-15} top="70%" left="70%" src={purple_shorts} alt="purple shorts" />
+        <ParallaxItem scale={2} value={-15} top="20%" left="70%" src={purple_shorts} alt="purple shorts" />
+        <ParallaxItem scale={0.8} value={5} top="60%" left="80%" src={purple_shorts} alt="purple shorts" />
+        <ParallaxItem scale={1} value={30} top="40%" left="60%" src={purple_shorts} alt="purple shorts" />
+        <ParallaxItem scale={2.5} value={15} top="70%" left="20%" src={purple_shorts} alt="purple shorts" />
+        <ParallaxItem scale={1.5} value={15} top="80%" left="40%" src={purple_shorts} alt="purple shorts" />
+        <ParallaxItem scale={1} value={5} top="40%" left="30%" src={purple_shorts} alt="purple shorts" />
       </div>
-    </div>
-    <div className="parallax">
-      <ParallaxItem scale={2} value={-15} top="70%" left="70%" src={purple_shorts} alt="purple shorts" />
-      <ParallaxItem scale={2} value={-15} top="20%" left="70%" src={purple_shorts} alt="purple shorts" />
-      <ParallaxItem scale={0.8} value={5} top="60%" left="80%" src={purple_shorts} alt="purple shorts" />
-      <ParallaxItem scale={1} value={30} top="40%" left="60%" src={purple_shorts} alt="purple shorts" />
-      <ParallaxItem scale={2.5} value={15} top="70%" left="20%" src={purple_shorts} alt="purple shorts" />
-      <ParallaxItem scale={1.5} value={15} top="80%" left="40%" src={purple_shorts} alt="purple shorts" />
-      <ParallaxItem scale={1} value={5} top="40%" left="30%" src={purple_shorts} alt="purple shorts" />
-    </div>
-  </header>
+    </header>
   )
 }
 
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,4 +1,4 @@
-import './Main.css'
+import $style from './Main.module.css'
 
 import { useRef } from 'react'
 import Tab from '@/components/Tab'
@@ -15,8 +15,8 @@ function Main(): JSX.Element {
   const inViewport = useIntersection(exportElRef)
 
   return (
-    <main className="main" id="main">
-      <section className="edit">
+    <main className={$style.main} id="main">
+      <section className={$style.edit}>
         <Tab data={{ footwear, hats, tops, bottoms }}>
           <TabContent title="hats" list={hats} />
           <TabContent title="tops" list={tops} />
@@ -24,20 +24,20 @@ function Main(): JSX.Element {
           <TabContent title="shoes" list={footwear} />
         </Tab>
       </section>
-      <section className="user-container">
-        <div className={`user${inViewport ? ' _overview' : ''}`}>
-          <div className="user-info">
-            <div className="user-hair"></div>
-            <div className="user-skin"></div>
-            <div className="user-eyes"></div>
-            <div className="user-accessories"></div>
+      <section className={$style.user_container}>
+        <div className={`${$style.user}${inViewport ? $style._overview : ''}`}>
+          <div className={$style.user_info}>
+            <div className={$style.user_hair}></div>
+            <div className={$style.user_skin}></div>
+            <div className={$style.user_eyes}></div>
+            <div className={$style.user_accessories}></div>
           </div>
-          <div className="user-display"></div>
-          <div className="user-outfit">
-            <div className="user-hat" data-name="hats"></div>
-            <div className="user-top" data-name="tops"></div>
-            <div className="user-bottom" data-name="bottoms"></div>
-            <div className="user-shoes" data-name="shoes"></div>
+          <div className={$style.user_display}></div>
+          <div className={$style.user_outfit}>
+            <div className={$style.user_hat} data-name="hats"></div>
+            <div className={$style.user_top} data-name="tops"></div>
+            <div className={$style.user_bottom} data-name="bottoms"></div>
+            <div className={$style.user_shoes} data-name="shoes"></div>
           </div>
         </div>
       </section>
